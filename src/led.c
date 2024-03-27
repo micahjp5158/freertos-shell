7abf82007d422740770403669d3ec41cdbd034e7
Micah Perry <micahjp5158@gmail.com>
2024-03-26 23:03:01 -0400

    Add LED lookup table, SW timers, and timer clbk
    Adds an LED index enumerator and lookup table so LED pins can be
    accessed from an ID value.
    
    Sets up a SW timer for each LED. These timers currently have an
    infinitely long interval. Future changes will allow the user to update
    these intervals via console commands.
    
    Adds an LED SW timer callback to toggle the appropriate LED on / off.

--- src/led.c
+++ src/led.c
@@ -12,6 +12,9 @@
  ************************************/
 #include "led.h"
 
+#include "FreeRTOS.h"
+#include "timers.h"
+
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal.h"
 
@@ -24,6 +27,15 @@
 /************************************
  * PRIVATE MACROS AND DEFINES
  ************************************/
+// LED indexes
+enum LED_INDEX_T {
+  LED_GREEN_INDEX,
+  LED_ORANGE_INDEX,
+  LED_RED_INDEX,
+  LED_BLUE_INDEX,
+  NUM_LED_INDEX
+} LED_INDEX_T;
+
 // LED defines
 #define LED_CLK_ENABLE()  __HAL_RCC_GPIOD_CLK_ENABLE()
 
@@ -44,6 +56,11 @@
 /************************************
  * STATIC VARIABLES
  ************************************/
+// LED lookup table
+static const uint16_t LED_LOOKUP[NUM_LED_INDEX] = {LED_GREEN, LED_ORANGE, LED_RED, LED_BLUE};
+
+// Timers used for LED toggle functions
+TimerHandle_t LED_Timers[NUM_LED_INDEX];
 
 /************************************
  * GLOBAL VARIABLES
@@ -53,6 +70,7 @@
  * STATIC FUNCTION PROTOTYPES
  ************************************/
 static void led_gpio_init(uint16_t pin);
+static void led_timer_callback(TimerHandle_t xTimer);
 
 /************************************
  * STATIC FUNCTIONS
@@ -68,6 +86,20 @@ static void led_gpio_init(uint16_t pin)
   HAL_GPIO_WritePin(LED_GPIO_PORT, pin, GPIO_PIN_RESET);
 }
 
+static void led_timer_callback(TimerHandle_t xTimer)
+{
+  // Select LED based on timer ID
+  uint32_t id = (uint32_t) pvTimerGetTimerID(xTimer);
+  if (id >= NUM_LED_INDEX)
+  {
+    // TODO error handling
+    return;
+  }
+
+  // Toggle the selected pin
+  HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_LOOKUP[id]);
+}
+
 /************************************
  * GLOBAL FUNCTIONS
  ************************************/
@@ -77,8 +109,20 @@ void led_init(void)
   LED_CLK_ENABLE();
 
   // Initialize each LED pin
-  led_gpio_init(LED_GREEN);
-  led_gpio_init(LED_ORANGE);
-  led_gpio_init(LED_RED);
-  led_gpio_init(LED_BLUE);
+  for (int i = 0; i < NUM_LED_INDEX; i++)
+  {
+    led_gpio_init(LED_LOOKUP[i]);
+  }
+
+  // Set up LED software timers
+  LED_Timers[LED_GREEN_INDEX] = xTimerCreate("Green LED timer", portMAX_DELAY, pdTRUE, (void *)LED_GREEN_INDEX, led_timer_callback);
+  LED_Timers[LED_ORANGE_INDEX] = xTimerCreate("Orange LED timer", portMAX_DELAY, pdTRUE, (void *)LED_ORANGE_INDEX, led_timer_callback);
+  LED_Timers[LED_RED_INDEX] = xTimerCreate("Red LED timer", portMAX_DELAY, pdTRUE, (void *)LED_RED_INDEX, led_timer_callback);
+  LED_Timers[LED_BLUE_INDEX] = xTimerCreate("Blue LED timer", portMAX_DELAY, pdTRUE, (void *)LED_BLUE_INDEX, led_timer_callback);
+
+  // Start the LED software timers
+  for (int i = 0; i < NUM_LED_INDEX; i++)
+  {
+    xTimerStart(LED_Timers[i], portMAX_DELAY);
+  }
 }
